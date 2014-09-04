b105040ca5fd89f949a67582953de8c9e00eab24
howandhao@163.com <howandhao@163.com>
2014-09-04 19:20:08 +0800

    Initial Commit

---
+++ Top Regions/Top Regions/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Top Regions
+//
+//  Created by appleseed on 9/4/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Top Regions/Top Regions/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Top Regions
+//
+//  Created by appleseed on 9/4/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
